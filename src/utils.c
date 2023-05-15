3786a98f86fb2976cf2d43dcf2aed9d7e553359e
lky <597572246@qq.com>
2023-05-15 15:47:08 +0000

    feat: add `get_content` to read file

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,12 @@
 #include "utils.h"
+
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
 int copy_string(char *cnf, const char *arg) {
     int len = strlen(arg);
@@ -20,4 +25,23 @@ int copy_number(int *cnf, const char *arg) {
     // something wrong when convert
     if (errno != 0 || *endptr != 0) return 2;
     return 0;
+}
+
+char* get_content(const char *file, int *len) {
+    int fd, e;
+    struct stat s;
+    char *data;
+    fd = open(file, O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "Open %s: %s\n", file, strerror(errno));
+        return NULL;
+    }
+    e = fstat(fd, &s);
+    if (e < 0) {
+        perror("Get file stat");
+        return NULL;
+    }
+    data = calloc(s.st_size + 5, 1);
+    *len = read(fd, data, s.st_size + 5);
+    return data;
 }
