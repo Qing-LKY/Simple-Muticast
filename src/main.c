02b034587695fac4f692eec9c95a65840f70da03
lky <597572246@qq.com>
2023-05-16 02:32:45 +0000

    feat: call client

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
 #include "args.h"
 #include "server.h"
+#include "client.h"
 
 int main(int argc, char *argv[]) {
     int e;
     e = parse_args(argc, argv);
     if (is_server) server_main();
-    else {
-        
-    }
+    else client_main();
     return 0;
 }
