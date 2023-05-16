486d94e959147b682ee1a440562bc6eb39487e51
lky <597572246@qq.com>
2023-05-16 06:34:19 +0000

    fix: unused value e

--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 #include "server.h"
 #include "client.h"
 
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
     int e;
     e = parse_args(argc, argv);
+    if (e < 0) exit(EXIT_FAILURE);
     if (is_server) server_main();
     else client_main();
     return 0;
